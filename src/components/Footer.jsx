c1470f21cddf9913a05b623ded428411208f5073
Nathakarn <nathakarn49@gmail.com>
2024-03-30 01:10:47 +0700

    add: header hero footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 
 const Footer = () =>{
-    return <div>
-        Footer xdxd
-    </div>
+    return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center"
+        >Coptright &copy; Ecommerce Shop 2024. All rights reserved
+        </p>
+      </div>
+        
+    </footer>
+    );
 };
 export default Footer;
